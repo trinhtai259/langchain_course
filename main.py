71ecb276ac39ec715ba648e695402236e381117f
Trinh Van Tai <vantai259@gmail.com>
2026-02-28 15:45:23 +0700

    day1

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from dotenv import load_dotenv
-
+from langchain_core.prompts import PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAI
 
 load_dotenv()
+
+loader = PyPDFLoader("C:\AI_Engineer\legal_chatbot\data\luat_an_ninh_mang.pdf")
+pages = loader.load_and_split()
+information = pages[0].page_content
+summary_template = """
+Bạn là một trợ lý hữu ích tóm tắt nội dung của tài liệu PDF.
+
+Đây là nội dung của tài liệu PDF: {information}
+Vui lòng cung cấp bản tóm tắt ngắn gọn các điểm chính và thông tin quan trọng từ tài liệu."""
+summary = PromptTemplate.from_template( template=summary_template)
+
+llm = GoogleGenerativeAI(model="gemini-3-flash-preview", temperature=0.3)
+
+chain = summary | llm
+response = chain.invoke(input={'information':information})
 def main():
-    print("Hello from legal-chatbot!")
+    print(response)
 
 
 if __name__ == "__main__":
